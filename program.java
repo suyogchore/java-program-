a0c0b52be87cef17e3b5822b7fc675a1d6013703
Suyog Chore <2024000622@mssu.ac.in>
2025-12-20 00:32:35 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    int age;
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        id = sc.nextInt();
+
+        sc.nextLine(); // clear buffer
+
+        System.out.print("Enter Student Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Student Age: ");
+        age = sc.nextInt();
+    }
+
+    void display() {
+        System.out.println("\n--- Student Details ---");
+        System.out.println("ID   : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+    }
+}
+
+public class StudentManagement {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.input();
+        s.display();
+
+        System.out.println("\nProgram executed successfully.");
+    }
+}
